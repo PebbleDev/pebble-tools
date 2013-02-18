169259f7d7a87e3ef3dd476404af5a5c2f9346dc
Jens Andersen <jens.andersen@gmail.com>
2013-02-18 09:41:53 +0000

    Fill in recently discovered fields

--- appinfo.c
+++ appinfo.c
@@ -7,18 +7,18 @@ typedef struct
 {
   char magic[8]; // PBLAPP\0\0
   unsigned short version;
-  unsigned short unknown1; // another version?
-  unsigned short unknown2;
+  unsigned short sdk_version;
+  unsigned short app_version;
   unsigned short size;
   unsigned int entry_point;
   unsigned int crc;
   char name[32];
   char company[32];
   unsigned int unknown3; // Always seems to be one?
-  unsigned int unknown4; // Some offset in file
-  unsigned int unknown5;
-  unsigned int unknown6; // Some offset in file
-  unsigned int unknown7;
+  unsigned int jump_table; // offset in file
+  unsigned int flags;
+  unsigned int reloc_list; // offset in file
+  unsigned int num_relocs;
 } pblapp;
 
 int main(int argc, char **argv)
@@ -45,18 +45,18 @@ int main(int argc, char **argv)
 
   printf("magic: %.*s\n", 8, hdr.magic);
   printf("Version: %d\n", hdr.version);
-  printf("unknown1: %d\n", hdr.unknown1);
-  printf("unknown2: %d\n", hdr.unknown2);
+  printf("Sdk Version: %d\n", hdr.sdk_version);
+  printf("App Version: %d\n", hdr.app_version);
   printf("size: %d (0x%x)\n", hdr.size, hdr.size);
-  printf("entry_point: %u (0x%x)\n", hdr.entry_point, hdr.entry_point);
+  printf("Entry Point: %u (0x%x)\n", hdr.entry_point, hdr.entry_point);
   printf("crc: %u\n", hdr.crc);
   printf("name: %.*s\n", 32, hdr.name);
   printf("company: %.*s\n", 32, hdr.company);
   printf("unknown3: %d\n", hdr.unknown3);
-  printf("unknown4: %u (0x%x)\n", hdr.unknown4, hdr.unknown4);
-  printf("unknown5: %u\n", hdr.unknown5);
-  printf("unknown6: %d (0x%x)\n", hdr.unknown6, hdr.unknown6);
-  printf("unknown7: %d\n", hdr.unknown7);
+  printf("jump_table address: %u (0x%x)\n", hdr.jump_table, hdr.jump_table);
+  printf("flags: %u\n", hdr.flags);
+  printf("relocation list address: %d (0x%x)\n", hdr.reloc_list, hdr.reloc_list);
+  printf("number of relocations: %d\n", hdr.num_relocs);
 
   error_read:
     fclose(file);
